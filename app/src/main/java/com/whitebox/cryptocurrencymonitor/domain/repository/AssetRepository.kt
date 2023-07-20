70f67d414c298872deb89fbaacaa243caf494a1f
Patrick Essiam <pat.essiam701@gmail.com>
2023-07-20 19:25:41 +0000

    Remove unused import

--- app/src/main/java/com/whitebox/cryptocurrencymonitor/domain/repository/AssetRepository.kt
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/domain/repository/AssetRepository.kt
@@ -1,10 +1,8 @@
 package com.whitebox.cryptocurrencymonitor.domain.repository
 
-import com.whitebox.cryptocurrencymonitor.common.WorkResult
 import com.whitebox.cryptocurrencymonitor.domain.model.Asset
 import com.whitebox.cryptocurrencymonitor.domain.model.AssetIcon
 import com.whitebox.cryptocurrencymonitor.domain.model.ExchangeRate
-import kotlinx.coroutines.flow.Flow
 
 interface AssetRepository {
 
