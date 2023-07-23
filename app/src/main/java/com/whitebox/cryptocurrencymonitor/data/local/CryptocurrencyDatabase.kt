71d19f742a0a447134d1678e3d45e0b40696ccbf
Patrick Essiam <pat.essiam701@gmail.com>
2023-07-23 02:04:36 +0000

    Add Room Database class

---
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/data/local/CryptocurrencyDatabase.kt
@@ -0,0 +1,14 @@
+package com.whitebox.cryptocurrencymonitor.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.whitebox.cryptocurrencymonitor.data.local.entity.AssetEntity
+import com.whitebox.cryptocurrencymonitor.data.local.entity.ExchangeRateEntity
+
+@Database(
+    entities = [AssetEntity::class, ExchangeRateEntity::class],
+    version = 1
+)
+abstract class CryptocurrencyDatabase: RoomDatabase() {
+    abstract fun cryptocurrencyDao(): CryptocurrencyDao
+}
