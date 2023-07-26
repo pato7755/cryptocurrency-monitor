972364928795d58753a99ec1494f39b605d7d2ff
Patrick Essiam <pat.essiam701@gmail.com>
2023-07-26 17:18:10 +0000

    Refactor Asset domain model to add iconUrl

--- app/src/main/java/com/whitebox/cryptocurrencymonitor/domain/model/Asset.kt
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/domain/model/Asset.kt
@@ -4,6 +4,6 @@ package com.whitebox.cryptocurrencymonitor.domain.model
 data class Asset(
     val assetId: String,
     val name: String,
-    val typeIsCrypto: Int
+    val typeIsCrypto: Int,
+    val iconUrl: String? = null
 )
-
