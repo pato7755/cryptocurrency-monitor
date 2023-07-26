3ebe102406c21bfa9c5e7595536de8a9d0e4c62a
Patrick Essiam <pat.essiam701@gmail.com>
2023-07-26 17:36:51 +0000

    Show AssetScreen on MainActivity

--- app/src/main/java/com/whitebox/cryptocurrencymonitor/MainActivity.kt
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.whitebox.cryptocurrencymonitor.ui.assets.AssetScreen
 import com.whitebox.cryptocurrencymonitor.ui.theme.CryptocurrencyMonitorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +24,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AssetScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CryptocurrencyMonitorTheme {
-        Greeting("Android")
+        AssetScreen()
     }
 }
