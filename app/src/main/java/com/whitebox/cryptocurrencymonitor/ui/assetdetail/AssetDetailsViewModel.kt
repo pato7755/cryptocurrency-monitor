428b29501e0e76b7a76bee5e5a1f521ca0c7bbd9
Patrick Essiam <pat.essiam701@gmail.com>
2023-08-01 17:08:30 +0000

    Check for internet connection on asset details screen before fetching data

--- app/src/main/java/com/whitebox/cryptocurrencymonitor/ui/assetdetail/AssetDetailsViewModel.kt
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/ui/assetdetail/AssetDetailsViewModel.kt
@@ -6,11 +6,16 @@ import androidx.lifecycle.viewModelScope
 import com.whitebox.cryptocurrencymonitor.common.WorkResult
 import com.whitebox.cryptocurrencymonitor.domain.usecase.asset.GetAssetDetailsUseCase
 import com.whitebox.cryptocurrencymonitor.domain.usecase.exchangerate.GetExchangeRateUseCase
+import com.whitebox.cryptocurrencymonitor.util.NetworkConnectivityService
+import com.whitebox.cryptocurrencymonitor.util.NetworkStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,6 +25,7 @@ class AssetDetailsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val getAssetDetailsUseCase: GetAssetDetailsUseCase,
     private val getExchangeRateUseCase: GetExchangeRateUseCase,
+    networkConnectivityService: NetworkConnectivityService
 ) : ViewModel() {
     private val _assetDetailsState = MutableStateFlow(AssetDetailsState())
     val assetDetailsState = _assetDetailsState.asStateFlow()
@@ -27,10 +33,24 @@ class AssetDetailsViewModel @Inject constructor(
     private val _exchangeRateState = MutableStateFlow(ExchangeRateState())
     val exchangeRateState = _exchangeRateState.asStateFlow()
 
+    private val _networkConnectivityState: StateFlow<NetworkStatus> =
+        networkConnectivityService.networkStatus.stateIn(
+            initialValue = NetworkStatus.Unknown,
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(3000)
+        )
+
     init {
         val assetId = savedStateHandle.get<String>("assetId") ?: ""
-        getAssetDetails(assetId = assetId)
-        getExchangeRate(assetId = assetId)
+        _networkConnectivityState.value.let { status ->
+            if (status is NetworkStatus.Connected) {
+                getAssetDetails(assetId = assetId)
+                getExchangeRate(assetId = assetId)
+            } else {
+                getAssetDetails(assetId = assetId, fetchFromRemote = false)
+                getExchangeRate(assetId = assetId, fetchFromRemote = false)
+            }
+        }
     }
 
     private fun getAssetDetails(assetId: String, fetchFromRemote: Boolean = true) {
