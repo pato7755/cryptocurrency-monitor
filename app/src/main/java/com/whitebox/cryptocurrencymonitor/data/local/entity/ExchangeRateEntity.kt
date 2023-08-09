b3470179c92a2ac83cbdbed01f10ab149406ad13
Patrick Essiam <pat.essiam701@gmail.com>
2023-08-09 07:45:31 +0000

    Make asset_id_base a unique column to avoid duplicate insertions

--- app/src/main/java/com/whitebox/cryptocurrencymonitor/data/local/entity/ExchangeRateEntity.kt
+++ app/src/main/java/com/whitebox/cryptocurrencymonitor/data/local/entity/ExchangeRateEntity.kt
@@ -2,9 +2,10 @@ package com.whitebox.cryptocurrencymonitor.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "exchange_rate")
+@Entity(tableName = "exchange_rate", indices = [Index(value = ["asset_id_base"], unique = true)])
 data class ExchangeRateEntity(
     @ColumnInfo(name = "asset_id_base")
     val assetIdBase: String,
